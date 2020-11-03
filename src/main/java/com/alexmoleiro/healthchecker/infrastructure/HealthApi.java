0d5cd2377fc3c5e6e36e596722d8cb974f0dd7d2
Alex Moleiro <alex@caucana.com>
2020-11-03 22:29:09 +0100

    Http client (#1)
    * Clean up and README.md
    
    * CORS
    
    Co-authored-by: A. Moleiro <alejandro.moleiro@schibsted.com>

--- src/main/java/com/alexmoleiro/healthchecker/infrastructure/HealthApi.java
+++ src/main/java/com/alexmoleiro/healthchecker/infrastructure/HealthApi.java
@@ -1,5 +1,6 @@
 package com.alexmoleiro.healthchecker.infrastructure;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthApi {
 
 
+  @CrossOrigin(origins = "http://localhost:8080")
   @PostMapping("/status")
   WebStatusResponse webStatusResult(@RequestBody WebStatusRequest webStatusRequest) {
-      return new WebStatusResponse("UP", webStatusRequest.getUrl(), 200);
+      return new WebStatusResponse(webStatusRequest);
   }
 }

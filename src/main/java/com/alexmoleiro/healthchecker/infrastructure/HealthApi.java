15d5c53e89aaca8f83a7a3814db778bfdefd31f2
A. Moleiro <alejandro.moleiro@schibsted.com>
2020-11-03 22:28:25 +0100

    CORS

--- src/main/java/com/alexmoleiro/healthchecker/infrastructure/HealthApi.java
+++ src/main/java/com/alexmoleiro/healthchecker/infrastructure/HealthApi.java
@@ -1,5 +1,6 @@
 package com.alexmoleiro.healthchecker.infrastructure;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthApi {
 
 
+  @CrossOrigin(origins = "http://localhost:8080")
   @PostMapping("/status")
   WebStatusResponse webStatusResult(@RequestBody WebStatusRequest webStatusRequest) {
       return new WebStatusResponse(webStatusRequest);

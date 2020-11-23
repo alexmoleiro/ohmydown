14229705b088aed776f88ace4d63559708a65866
Alex Moleiro <alex@caucana.com>
2020-11-23 09:47:45 +0100

    Timeout returns 408 (#8)

--- src/test/java/com/alexmoleiro/healthchecker/client/HttpClientTest.java
+++ src/test/java/com/alexmoleiro/healthchecker/client/HttpClientTest.java
@@ -3,15 +3,16 @@ package com.alexmoleiro.healthchecker.client;
 import com.alexmoleiro.healthchecker.core.WebStatusRequest;
 import com.alexmoleiro.healthchecker.infrastructure.WebStatusRequestDto;
 import com.alexmoleiro.healthchecker.service.SiteChecker;
+import com.alexmoleiro.healthchecker.service.SiteCheckerException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
@@ -19,6 +20,9 @@ import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static java.net.http.HttpClient.newBuilder;
+import static java.time.Duration.ofMillis;
+import static java.time.Duration.ofSeconds;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -29,15 +33,35 @@ public class HttpClientTest {
   private HttpClient client = newBuilder().build();
 
   @Test
-  void shouldCallSite() throws IOException, InterruptedException, URISyntaxException {
+  void shouldCallSite() throws InterruptedException, URISyntaxException {
 
-    stubFor(get(urlEqualTo("/log")));
+    stubFor(get(urlEqualTo("/log")).willReturn(aResponse().withStatus(200)));
 
-    final WebStatusRequestDto mock = mock(WebStatusRequestDto.class);
-    when(mock.getUrl()).thenReturn("http://localhost:8765/log");
+    final WebStatusRequestDto webStatusRequestDtoMock = mock(WebStatusRequestDto.class);
+    when(webStatusRequestDtoMock.getUrl()).thenReturn("http://localhost:8765/log");
 
-    new SiteChecker(client).check(new WebStatusRequest(mock));
+    new SiteChecker(client, ofSeconds(2)).check(new WebStatusRequest(webStatusRequestDtoMock));
 
     verify(getRequestedFor(urlMatching("/log")));
+  }
+
+  @Test
+  void shouldThrowExceptionWhenTimeOutExceeded() {
+
+    final int serverDelay = 1 * 1000;
+    final int clientTimeout = 500;
+
+    stubFor(
+        get(urlEqualTo("/log")).willReturn(aResponse().withStatus(200).withFixedDelay(serverDelay)));
+
+    final WebStatusRequestDto webStatusRequestDtoMock = mock(WebStatusRequestDto.class);
+    when(webStatusRequestDtoMock.getUrl()).thenReturn("http://localhost:8765/log");
+
+    assertThatThrownBy(
+            () ->
+                new SiteChecker(client, ofMillis(clientTimeout))
+                    .check(new WebStatusRequest(webStatusRequestDtoMock)))
+        .isInstanceOf(SiteCheckerException.class)
+        .hasMessageContaining("request timed out");
   }
 }

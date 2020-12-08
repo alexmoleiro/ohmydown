a54a33f77df8cb8965ad6cd3c885b59be1138aa6
Alex Moleiro <alex@caucana.com>
2020-12-08 11:18:59 +0100

    Random User Agents (#28)

--- src/main/java/com/alexmoleiro/healthchecker/core/UserAgent.java
+++ src/main/java/com/alexmoleiro/healthchecker/core/UserAgent.java
@@ -1,17 +1,26 @@
 package com.alexmoleiro.healthchecker.core;
 
+import java.util.Random;
+
 public enum UserAgent {
-  MOZILLA(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+  WIN10("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"),
+  FIREFOX("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0"),
+  IPHONE("Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"),
+  SAFARI("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.1 Safari/605.1.15"),
+  EDGE("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 Edg/86.0.622.69");
 
-  private final String string;
 
-  UserAgent(String string) {
+  private final String userAgentString;
 
-    this.string = string;
+  UserAgent(String userAgentString) {
+    this.userAgentString = userAgentString;
   }
 
-  public String getValue() {
-    return string;
+  public static String random() {
+    return values()[new Random().nextInt(values().length)].getValue();
+  }
+
+  private String getValue() {
+    return userAgentString;
   }
 }

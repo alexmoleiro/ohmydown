f5e05b16671b40b4088de5fdaa92a1981571f659
A. Moleiro <alejandro.moleiro@schibsted.com>
2021-01-08 18:42:18 +0100

    Uptime. Invalid ticks / valid ticks

--- src/main/java/com/alexmoleiro/healthchecker/core/healthCheck/HealthCheckResponses.java
+++ src/main/java/com/alexmoleiro/healthchecker/core/healthCheck/HealthCheckResponses.java
@@ -1,12 +1,16 @@
 package com.alexmoleiro.healthchecker.core.healthCheck;
 
-import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+import static java.lang.Float.valueOf;
+import static org.springframework.http.HttpStatus.OK;
 
 public class HealthCheckResponses {
 
   private final Endpoint endpoint;
-  private  LinkedList<HealthCheckResponse> healthCheckResponses = new LinkedList<>();
+  private LinkedList<HealthCheckResponse> healthCheckResponses = new LinkedList<>();
 
   public HealthCheckResponses(Endpoint endpoint, HealthCheckResponse response) {
     this.endpoint = endpoint;
@@ -29,7 +33,14 @@ public class HealthCheckResponses {
     healthCheckResponses.addLast(response);
   }
 
-  public BigDecimal getUptime() {
-    return BigDecimal.valueOf(50.00);
+  public float getUptime() {
+    float totalTicks = healthCheckResponses.size();
+    float totalNoOk =
+        healthCheckResponses.stream()
+            .filter(r -> r.getStatus() != OK.value())
+            .collect(Collectors.toList())
+            .size();
+    final DecimalFormat decimalFormat = new DecimalFormat("##.##");
+    return valueOf(decimalFormat.format((totalTicks - totalNoOk) / totalTicks * 100));
   }
 }
--- src/test/java/com/alexmoleiro/healthchecker/core/healthCheck/HealthCheckResponsesTest.java
+++ src/test/java/com/alexmoleiro/healthchecker/core/healthCheck/HealthCheckResponsesTest.java
@@ -1,11 +1,13 @@
 package com.alexmoleiro.healthchecker.core.healthCheck;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.OK;
 
 
 class HealthCheckResponsesTest {
@@ -14,12 +16,18 @@ class HealthCheckResponsesTest {
   void shouldReturnUptime() {
     final Endpoint endpoint = new Endpoint(new HttpUrl("https://www.as.com"));
     final LocalDateTime now = LocalDateTime.now();
-    final HealthCheckResponse response = new HealthCheckResponse(endpoint.getHttpUrl(), HttpStatus.OK.value(), now, now);
-    final HealthCheckResponse response2 = new HealthCheckResponse(endpoint.getHttpUrl(), HttpStatus.FORBIDDEN.value(), now, now);
+    final HealthCheckResponses healthCheckResponses = new HealthCheckResponses(endpoint);
 
-    final HealthCheckResponses healthCheckResponses = new HealthCheckResponses(endpoint, response);
-    healthCheckResponses.addLast(response2);
+    HealthCheckResponse response = new HealthCheckResponse(endpoint.getHttpUrl(), FORBIDDEN.value(), now, now);
+    healthCheckResponses.addLast(response);
 
-    Assertions.assertThat(healthCheckResponses.getUptime()).isEqualTo(BigDecimal.valueOf(50.00));
+    IntStream.rangeClosed(1,287).forEach(i-> {
+      final HealthCheckResponse response2 =
+          new HealthCheckResponse(endpoint.getHttpUrl(), OK.value(), now, now);
+      healthCheckResponses.addLast(response2);
+    });
+
+
+    assertThat(healthCheckResponses.getUptime()).isEqualTo(99.65f);
   }
 }
